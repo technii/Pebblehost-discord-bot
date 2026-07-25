3b36f82ed20a35f50d6a8085c091cea6a749669c
techni <145453745+technii@users.noreply.github.com>
2026-07-25 18:29:15 +0100

    added some local stuff but it doesnt seem to work due to the api not liking it

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -2,14 +2,16 @@ from flask import Flask, request, jsonify, render_template, make_response, redir
 import Shared
 from SQLcursor import db,sqlcursor,sqlqueries
 from datetime import datetime, timezone
+import datetime as dt
 import requests
+import json
 app = Flask(__name__)
-images = ["https://cdn.discordapp.com/attachments/1265740184931143785/1510412056464195764/fatbilly.gif?ex=6a1cb858&is=6a1b66d8&hm=8dd4e3454ec84a77cc2e381e63a2d4f6c29e2cb34e2f4a260ae0f7d2e255f99f&","https://cdn.discordapp.com/attachments/755066782032724019/1509944722880204821/Holliday_Dave.png?ex=6a1c569b&is=6a1b051b&hm=75fc3e4b7ffbe554a86ed654fbb99f9543713ae730d93b14e051b044e6e607c2&","https://cdn.discordapp.com/attachments/1231286447131328544/1428093055713546401/viscous.gif?ex=6a1c834d&is=6a1b31cd&hm=6de58578bd86f49417f150b260eef2e0843fa5ad7c94aa03a8f740e5b74201ac&","https://cdn.discordapp.com/attachments/755066782032724019/1507070248182874243/image.png?ex=6a1c6d8b&is=6a1b1c0b&hm=70acabae4247b3ea74453c59928a06a8b37ad005a67a7fb2efa46e6bbd8f5e4b&","https://cdn.discordapp.com/attachments/755066782032724019/1505241591684530266/IMG_6383.jpg?ex=6a1c5df9&is=6a1b0c79&hm=6e99c6be366bde9260de83fa18c154bc70c7ccbc3c524ce716c9cda5d99e518b&","https://cdn.discordapp.com/attachments/755066782032724019/1422574535730921513/IMG_5691.jpg?ex=6a1c3647&is=6a1ae4c7&hm=debf88ad82f1a9069d03045a7e7c0dec2f73173f9aecd755206be5010589f0c8&"]
 Discordapi = "https://discord.com/api/v10"
 app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024
 REDIRECT_URI = "https://api.dizzybot.online/handleoauth2loginpc"
+LOCALREDIRECT_URI = "http://localhost:8054/handleoauth2loginpc"
 clientid = "1510230840662167644"
-clientsecret = "rwvxnmUxyiRROAEDhYwTPHAqDUI944xW"
+clientsecret = "kHtfCh23tzsiSvIf8xLb0OLMy7hLMv4c"
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -90,14 +92,12 @@ def GetVoiceClient():
 
 @app.route("/handleoauth2loginpc")
 def HandleOauth2loginpc():
-    try:
-        code = request.args.get("code")
-        creds = getcreds(code)
-        resp = make_response("ForcedOauthRedirect.html")
-        resp.set_cookie("Test","Test")
-        return render_template(resp, fragment = jsonify(creds))
-    except Exception as e:
-        return render_template("error.html", error=str(e))
+    
+    code = request.args.get("code")
+    creds : dict = getcreds(code)
+    resp = make_response(render_template("ForcedOauthRedirect.html",fragment = json.dumps(creds)))
+    return resp
+    
 def getcreds(code):
     data = {
         'grant_type': 'authorization_code',
@@ -110,3 +110,37 @@ def getcreds(code):
     r = requests.post('%s/oauth2/token' % Discordapi, data=data, headers=headers, auth=(clientid, clientsecret))
     r.raise_for_status()
     return r.json()
+
+@app.route("/localhandleoauth2loginpc")
+def LocalHandleOauth2loginpc():
+    try:
+        code = request.args.get("code")
+        creds : dict = localgetcreds(code)
+        resp = make_response(render_template("ForcedOauthRedirect.html",fragment = json.dumps(creds)))
+        return resp
+    except Exception as e:
+        return render_template("errorpage.html",error = e)
+def localgetcreds(code):
+    data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': LOCALREDIRECT_URI
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    r = requests.post('%s/oauth2/token' % Discordapi, data=data, headers=headers, auth=(clientid, clientsecret))
+    r.raise_for_status()
+    return r.json()
+
+
+
+"""
+{
+  "access_token": "6qrZcUqja7812RVdnEKjpzOL4CvHBFG",
+  "token_type": "Bearer",
+  "expires_in": 604800,
+  "refresh_token": "D43f5y0ahjqew82jZ4NViEr2YafMKhue",
+  "scope": "identify"
+}
+"""
--- Main.py
+++ Main.py
@@ -6,8 +6,8 @@ from SQLcursor import main
 
 sql_thread = threading.Thread(target=main, daemon=True)
 sql_thread.start()
-bot_thread = threading.Thread(target=run_bot, daemon=True)
-bot_thread.start()
+#bot_thread = threading.Thread(target=run_bot, daemon=True)
+#bot_thread.start()
 print("Bot Started")
 print("SqlCursor Started")
 print("Started the server on 0.0.0.0 at port 8054")
